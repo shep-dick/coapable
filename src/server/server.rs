b98ee931344230e8cb77e3254db95c8da7ef743d
Shep Dick <shep@dick.org>
2026-02-24 09:58:47 -0500

    feat: wired router into server

--- src/server/server.rs
+++ src/server/server.rs
@@ -1,16 +1,37 @@
-use crate::transport::ServerInterface;
-use coap_lite::ContentFormat;
+use coap_lite::{ContentFormat, MessageType};
 
-use super::Result;
+use crate::message_types::CoapRequest;
+use crate::transport::ServerInterface;
+
+use super::router::Router;
 
 pub struct CoapServer {
     interface: ServerInterface,
+    router: Router,
 }
 
 impl CoapServer {
-    async fn run(&mut self) {
+    pub fn new(interface: ServerInterface, router: Router) -> Self {
+        Self { interface, router }
+    }
+
+    pub async fn run(&mut self) {
         loop {
-            let _ = self.interface.recv_request().await.unwrap();
+            let (packet, peer) = match self.interface.recv_request().await {
+                Ok(req) => req,
+                Err(_) => break,
+            };
+
+            let token = packet.get_token().to_vec();
+            let request = CoapRequest::from_raw(packet, peer);
+
+            let response = self.router.dispatch(request).await;
+
+            let mut response_packet = response.into_packet();
+            response_packet.set_token(token);
+            response_packet.header.set_type(MessageType::NonConfirmable);
+
+            let _ = self.interface.send_response(response_packet, peer).await;
         }
     }
 }
