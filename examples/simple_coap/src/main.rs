1a3192e125c6ff894657e0cbc9ff422d494310e8
Shep Dick <shep@dick.org>
2026-02-25 14:59:43 -0500

    examples: updated simple_coap

--- examples/simple_coap/src/main.rs
+++ examples/simple_coap/src/main.rs
@@ -1,6 +1,7 @@
-use coap_lite::ResponseType;
+use coap_lite::{ContentFormat, ResponseType};
 use coapable::{
-    CoapClient, CoapEndpoint, CoapRequest, CoapResponse, CoapServer, CoapStack, Router, get,
+    CoapClient, CoapEndpoint, CoapRequest, CoapResponse, CoapServer, CoapStack, RequestContext,
+    Router, get,
 };
 use tokio::net::UdpSocket;
 
@@ -8,14 +9,14 @@ use tokio::net::UdpSocket;
 async fn main() {
     let sock = UdpSocket::bind("127.0.0.1:5683").await.unwrap();
     let ep = CoapEndpoint::start(sock).await.unwrap();
-    let (client_if, server_if, task) = CoapStack::start(ep).await.unwrap();
+    let (client_if, server_if, _) = CoapStack::start(ep).await.unwrap();
 
     let client = CoapClient::new(client_if);
 
     let router = Router::new().route("/info", get(get_info));
     let server = CoapServer::new(server_if, router);
 
-    let server_task = tokio::spawn(server.run());
+    tokio::spawn(server.run());
 
     let response = client
         .get("127.0.0.1:5683".parse().unwrap())
@@ -24,11 +25,14 @@ async fn main() {
         .await
         .unwrap();
 
-    println!("Received response with code {:?}", response.status());
-
-    server_task.abort();
+    println!("Response code: {:?}", response.status());
+    println!("    Content Format: {:?}", response.content_format());
+    println!("    Payload: {:?}", response.payload());
 }
 
-async fn get_info(req: CoapRequest) -> CoapResponse {
-    CoapResponse::new(ResponseType::MethodNotAllowed).build()
+async fn get_info(_: CoapRequest, _: RequestContext) -> CoapResponse {
+    CoapResponse::new(ResponseType::Content)
+        .content_format(ContentFormat::TextPlain)
+        .payload(b"response")
+        .build()
 }
